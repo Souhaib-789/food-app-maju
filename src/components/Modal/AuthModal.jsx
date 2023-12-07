acd47819f547f18dd19b7f8036ddda876d3c0820
hamzailyas237 <hamzailyas237@gmail.com>
2023-12-07 17:24:07 +0500

    started integrating auth apis

--- src/components/Modal/AuthModal.jsx
+++ src/components/Modal/AuthModal.jsx
@@ -9,19 +9,58 @@ import { FaEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import styles from "./AuthModal.module.css";
+import apicall from "../../utils/axios";
 
 const AuthModal = (props) => {
   const [showLoginPassword, setShowLoginPassword] = useState(false);
   const [showSignupPassword, setShowSignupPassword] = useState(false);
+  const [signupData, setSignupData] = useState({
+    fullname: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
   const loginHandler = (e) => {
     e.preventDefault();
-    alert("Logged In");
+    const { value, name } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
   };
 
   const signupHandler = (e) => {
     e.preventDefault();
-    alert("Signed up");
+    const { value, name } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value,
+    });
+  };
+
+  const signup = async (e) => {
+    e.preventDefault();
+    try {
+      await apicall.post(`/signup`, signupData);
+    } catch (err) {
+      console.log("signup error", err);
+    }
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      await apicall.post(`/login`, loginData);
+    } catch (err) {
+      console.log("login error", err);
+    }
   };
 
   return (
@@ -40,12 +79,15 @@ const AuthModal = (props) => {
       <>
         <p>We'll check if you have an account</p>
         {props.login ? (
-          <form className={styles.input_container} onSubmit={loginHandler}>
+          <form className={styles.input_container} onSubmit={login}>
             <Input
               className={styles.auth_input}
               size="large"
               placeholder="Enter Email"
               prefix={<FaEnvelope color="#fe043c" />}
+              name="email"
+              onChange={loginHandler}
+              value={loginData.email}
             />
             <Input
               className={styles.auth_input}
@@ -66,16 +108,22 @@ const AuthModal = (props) => {
                   />
                 )
               }
+              name="password"
+              onChange={loginHandler}
+              value={loginData.password}
             />
             <button className={styles.user_button}>Login</button>
           </form>
         ) : (
-          <form className={styles.input_container} onSubmit={signupHandler}>
+          <form className={styles.input_container} onSubmit={signup}>
             <Input
               className={styles.auth_input}
               size="large"
               placeholder="Enter Fullname"
               prefix={<FaUser color="#fe043c" />}
+              name="fullname"
+              onChange={signupHandler}
+              value={signupData.fullname}
             />
 
             <Input
@@ -84,12 +132,18 @@ const AuthModal = (props) => {
               placeholder="Enter Phone"
               type="number"
               prefix={<FaPhoneAlt color="#fe043c" />}
+              name="phone"
+              onChange={signupHandler}
+              value={signupData.phone}
             />
             <Input
               className={styles.auth_input}
               size="large"
               placeholder="Enter Email"
               prefix={<FaEnvelope color="#fe043c" />}
+              name="email"
+              onChange={signupHandler}
+              value={signupData.email}
             />
             <Input
               className={styles.auth_input}
@@ -110,6 +164,9 @@ const AuthModal = (props) => {
                   />
                 )
               }
+              name="password"
+              onChange={signupHandler}
+              value={signupData.password}
             />
 
             <Input
@@ -131,6 +188,9 @@ const AuthModal = (props) => {
                   />
                 )
               }
+              name="confirmPassword"
+              onChange={signupHandler}
+              value={signupData.confirmPassword}
             />
 
             <button className={styles.user_button}>Sign up</button>
